6bb3355d4ef5c98daf93fad0c3ecd48d24c62784
Nikhil Nandan <nikhilnandan.pawankumar@kanini.com>
2022-12-13 11:29:34 +0530

    Added routing module - app-routing.module.ts and made needfull changes in app.module.ts also created a wrapper in home component which will host other components like header and footer. created about page and linked it to check Routing, which currently renders Routing works messages. configured routing to render home component on loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
